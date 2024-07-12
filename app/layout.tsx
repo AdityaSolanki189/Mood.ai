5cf3f06038fd5d6e5f3ccd660f3f0d3cfff2a6c3
Aditya Solanki <aditya2ss283@gmail.com>
2024-07-13 01:29:41 +0530

    add middleware and auth components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,30 @@
-import './globals.css';
+import { ClerkProvider } from '@clerk/nextjs';
 import { Inter as FontSans } from 'next/font/google';
+import './globals.css';
 
 import { cn } from '@/lib/utils';
 
 const fontSans = FontSans({
-  subsets: ['latin'],
-  variable: '--font-sans',
+    subsets: ['latin'],
+    variable: '--font-sans',
 });
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{ children: React.ReactNode }>) {
-  return (
-    <html lang="en" suppressHydrationWarning>
-      <head />
-      <body
-        className={cn(
-          'min-h-screen bg-background font-sans antialiased',
-          fontSans.variable
-        )}
-      >
-        {children}
-      </body>
-    </html>
-  );
+    return (
+        <ClerkProvider>
+            <html lang="en" suppressHydrationWarning>
+                <head />
+                <body
+                    className={cn(
+                        'min-h-screen bg-background font-sans antialiased',
+                        fontSans.variable
+                    )}
+                >
+                    {children}
+                </body>
+            </html>
+        </ClerkProvider>
+    );
 }
